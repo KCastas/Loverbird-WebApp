a9e32aadd19dcba3ff0d980ca3d7a38a06a14af3
KCastas <kchantsaris@hotmail.com>
2025-02-27 02:24:15 +0200

    Fix Firebase and iOS build issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
@@ -7,7 +7,7 @@ import 'theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
       child: const LoveBirdApp(),
